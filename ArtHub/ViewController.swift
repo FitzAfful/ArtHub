c7a3789a108d276bbd7caafa7f69517099165e0c
Fitgerald Afful <fitzafful@gmail.com>
2020-05-13 23:57:51 +0000

    Initial Commit

---
+++ ArtHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtHub
+//
+//  Created by Fitzgerald Afful on 13/05/2020.
+//  Copyright © 2020 Fitzgerald Afful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
